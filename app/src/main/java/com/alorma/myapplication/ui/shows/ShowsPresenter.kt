cd8b45d569735a68c4edc2120af4b203e6b8a25f
Bernat Borrás Paronella <bernatbor15@gmail.com>
2018-05-10 17:58:52 +0200

    CleanUp presenter calls for load

--- app/src/main/java/com/alorma/myapplication/ui/shows/ShowsPresenter.kt
+++ app/src/main/java/com/alorma/myapplication/ui/shows/ShowsPresenter.kt
@@ -4,6 +4,7 @@ import com.alorma.myapplication.commons.observeOnUI
 import com.alorma.myapplication.domain.usecase.ObtainShowsUseCase
 import com.alorma.myapplication.ui.common.BasePresenter
 import com.alorma.rac1.commons.plusAssign
+import io.reactivex.Single
 import javax.inject.Inject
 
 class ShowsPresenter @Inject constructor(private val states: ShowsStates,
@@ -13,27 +14,18 @@ class ShowsPresenter @Inject constructor(private val states: ShowsStates,
 
     override fun reduce(action: ShowsActions.ShowsAction) {
         when (action) {
-            ShowsActions.ShowsAction.Load -> onLoad()
-            ShowsActions.ShowsAction.LoadPage -> onLoadPage()
+            ShowsActions.ShowsAction.Load -> load(action)
+            ShowsActions.ShowsAction.LoadPage -> load(action)
             is ShowsActions.ShowsAction.OpenDetail -> onOpenDetail(action)
         }
     }
 
-    private fun onLoad() {
-        disposable += obtainShowsUseCase.execute()
-                .observeOnUI()
-                .doOnSubscribe { render(states loading true) }
-                .doOnSuccess { render(states loading false) }
-                .doOnError { render(states loading false) }
-                .subscribe({
-                    render(states success it)
-                }, {
-                    render(states error it)
-                })
-    }
-
-    private fun onLoadPage() {
-        disposable += obtainShowsUseCase.executeNextPage()
+    private fun load(action: ShowsActions.ShowsAction) {
+        disposable += when (action) {
+            is ShowsActions.ShowsAction.Load -> obtainShowsUseCase.execute()
+            is ShowsActions.ShowsAction.LoadPage -> obtainShowsUseCase.executeNextPage()
+            else -> Single.never()
+        }
                 .observeOnUI()
                 .doOnSubscribe { render(states loading true) }
                 .doOnSuccess { render(states loading false) }

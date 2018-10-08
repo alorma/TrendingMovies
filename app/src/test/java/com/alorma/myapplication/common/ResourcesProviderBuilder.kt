4e79de715f72d25233ee073b0250cd444817673a
Bernat Borrás Paronella <bernat.borras@privalia.com>
2018-10-08 17:49:53 +0200

    Fix test

--- app/src/test/java/com/alorma/myapplication/common/ResourcesProviderBuilder.kt
+++ app/src/test/java/com/alorma/myapplication/common/ResourcesProviderBuilder.kt
@@ -1,10 +1,10 @@
 package com.alorma.myapplication.common
 
-import com.alorma.presentation.common.ResourcesProvider
+import com.alorma.domain.common.ResourceProvider
 import com.nhaarman.mockito_kotlin.given
 import com.nhaarman.mockito_kotlin.mock
 import org.mockito.ArgumentMatchers
 
-fun getResourcesProvider(): ResourcesProvider = mock<ResourcesProvider>().apply {
+fun getResourcesProvider(): ResourceProvider = mock<ResourceProvider>().apply {
     given(getString(ArgumentMatchers.anyInt())).willReturn("")
 }

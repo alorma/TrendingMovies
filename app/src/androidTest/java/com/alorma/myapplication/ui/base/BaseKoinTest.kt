283c39711595eee57f59b96359eee16b680286cd
Bernat Borrás Paronella <bernatbor15@gmail.com>
2018-10-14 13:50:41 +0200

    Fix UI tests

--- app/src/androidTest/java/com/alorma/myapplication/ui/base/BaseKoinTest.kt
+++ app/src/androidTest/java/com/alorma/myapplication/ui/base/BaseKoinTest.kt
@@ -1,16 +1,11 @@
 package com.alorma.myapplication.ui.base
 
-import com.alorma.myapplication.config.configureRxThreading
 import org.junit.Before
 import org.koin.dsl.context.ModuleDefinition
 import org.koin.dsl.module.module
 import org.koin.standalone.StandAloneContext.loadKoinModules
 
 abstract class BaseKoinTest {
-
-    init {
-        configureRxThreading()
-    }
 
     @Before
     fun setup() {
